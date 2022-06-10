38e38a4e64a5814a2b3845459341aef62bab3e30
PatZino <eedawarez@gmail.com>
2022-06-10 04:42:26 +0100

    actual initial commit

---
+++ DsaPlayGround/PlayGround/PalindromeLargestNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsaPlayGround.PlayGround
+{
+    public static class PalindromeLargestNumber
+    {
+        /***
+         * 
+         * Find question description in the AllImages folder with the same 
+         * name as the class (PalindromeLargestNumber.jpeg)
+         * 
+         */
+        public static int LargestPalindrome(string nums)
+        {
+            if (string.IsNullOrEmpty(nums)) return 0;
+            if (Convert.ToInt32(nums) == 0) return 0;
+            if (nums.Length == 1) return Convert.ToInt32(nums);
+
+            string left = string.Empty;
+            string right = string.Empty;
+
+            HashSet<int> oddDig = new HashSet<int>();
+
+            int[] numFreq = new int[10];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var index = nums[i] - 48;
+                numFreq[index]++;
+            }
+
+            for (int i = 9; i >= 0; i--)
+            {
+                if (numFreq[i] % 2 == 0)
+                {
+                    var count = numFreq[i] / 2;
+                    while (count > 0)
+                    {
+                        left += (char)(i + 48);
+                        right = ((char)(i + 48)) + right;
+                        count--;
+                    }
+                }
+                else if (numFreq[i] > 2 && numFreq[i] % 2 == 1)
+                {
+                    oddDig.Add(i);
+                    var count = numFreq[i] / 2;
+                    while (count > 0)
+                    {
+                        left += (char)(i + 48);
+                        right = ((char)(i + 48)) + right;
+                        count--;
+                    }
+                }
+                else
+                {
+                    oddDig.Add(i);
+                }
+            }
+
+            var maxOddDig = (char)(oddDig.Max() + 48);
+            var result = left + maxOddDig + right;
+
+            return Convert.ToInt32(result);
+        }
+    }
+}
---
+++ DsaPlayGroundTest/PlayGroundTest/TestPalindromeLargestNumber.cs
@@ -0,0 +1,51 @@
+﻿using DsaPlayGround.PlayGround;
+using Xunit;
+
+namespace DsaPlayGroundTest.PlayGroundTest
+{
+    public class TestPalindromeLargestNumber
+    {
+        [Fact]
+        public void NullOrEmptyInput()
+        {
+            // Arrange
+            var input = "";
+            var expectedOutput = 0;
+
+            // Act
+            var actualOutput = PalindromeLargestNumber.LargestPalindrome(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void LengthOneInput()
+        {
+            // Arrange
+            var input = "7";
+            var expectedOutput = 7;
+
+            // Act
+            var actualOutput = PalindromeLargestNumber.LargestPalindrome(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void ValidInput()
+        {
+            // Arrange
+            var input = "2345625";
+            var expectedOutput = 52625;
+
+            // Act
+            var actualOutput = PalindromeLargestNumber.LargestPalindrome(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+    }
+}
