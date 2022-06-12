95014ec9f02e4a08576da5d0dd11c6b80e961b84
PatZino <eedawarez@gmail.com>
2022-06-12 19:47:15 +0100

    Added running sum of 1d array

---
+++ DsaPlayGround/Array/RunningSumOf1dArray.cs
@@ -0,0 +1,20 @@
+﻿namespace DsaPlayGround.Array
+{
+    public static class RunningSumOf1dArray
+    {
+        /***
+       * Find question description in the AllImages folder with the same 
+       * name as the class (RunningSumOf1dArray.png)
+       */
+
+        public static int[] RunningSum(int[] nums)
+        {
+            for (var i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+
+            return nums;
+        }
+    }
+}
---
+++ DsaPlayGroundTest/ArrayTest/TestRunningSumOf1dArray.cs
@@ -0,0 +1,22 @@
+﻿using DsaPlayGround.Array;
+using Xunit;
+
+namespace DsaPlayGroundTest.ArrayTest
+{
+    public class TestRunningSumOf1dArray
+    {
+        [Fact]
+        public void ValidInput()
+        {
+            // Arrange
+            var nums = new int[4] { 1, 2, 3, 4 };
+            var expectedOutput = new int[4] { 1, 3, 6, 10 };
+
+            // Act
+            var actualOutput = RunningSumOf1dArray.RunningSum(nums);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
