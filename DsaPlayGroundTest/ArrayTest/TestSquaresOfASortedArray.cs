425eaae3f77b93a8d7541da4a2f6e036134886db
Richkazz <edawarekaro@gmail.com>
2022-06-12 21:19:07 +0100

    Added Squares Of A Sorted Array

---
+++ DsaPlayGroundTest/ArrayTest/TestSquaresOfASortedArray.cs
@@ -0,0 +1,22 @@
+﻿using DsaPlayGround.Array;
+using Xunit;
+
+namespace DsaPlayGroundTest.ArrayTest
+{
+    public class TestSquaresOfASortedArray
+    {
+        [Fact]
+        public void ValidInput()
+        {
+            // Arrange
+            var nums = new int[] { -4, -1, 0, 3, 10 };
+            var expectedOutput = new int[] { 0, 1, 9, 16, 100 };
+
+            // Act
+            var actualOutput = SquaresOfASortedArray.SortedSquares(nums);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
