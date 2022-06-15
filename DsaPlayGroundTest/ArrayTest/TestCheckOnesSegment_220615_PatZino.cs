911ef712c325480a3c2e3ebddc3bf4d93e6d440a
PatZino <eedawarez@gmail.com>
2022-06-15 13:23:28 +0100

    Added check ones segment

---
+++ DsaPlayGroundTest/ArrayTest/TestCheckOnesSegment_220615_PatZino.cs
@@ -0,0 +1,36 @@
+﻿using DsaPlayGround.Array;
+using Xunit;
+
+namespace DsaPlayGroundTest.ArrayTest
+{
+    public class TestCheckOnesSegment_220615_PatZino
+    {
+        [Fact]
+        public void ValidInput()
+        {
+            // Arrange
+            var input = "110";
+            var expectedOutput = true;
+
+            // Act
+            var actualOutput = CheckOnesSegment_220615_PatZino.CheckOnesSegment(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void InValidInput()
+        {
+            // Arrange
+            var input = "1101";
+            var expectedOutput = false;
+
+            // Act
+            var actualOutput = CheckOnesSegment_220615_PatZino.CheckOnesSegment(input);
+
+            // Assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+    }
+}
