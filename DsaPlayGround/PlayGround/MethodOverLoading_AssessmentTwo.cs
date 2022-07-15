f7f0ba8541fce8e685d7ff96ba225e12ac44bcf3
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-07-15 22:45:30 +0100

    Update MethodOverLoading_AssessmentTwo.cs

--- DsaPlayGround/PlayGround/MethodOverLoading_AssessmentTwo.cs
+++ DsaPlayGround/PlayGround/MethodOverLoading_AssessmentTwo.cs
@@ -1,8 +1,38 @@
 ﻿using System;
 
 
-public enum Days { Moday, Tuesday, Wednessday, Thursday, Friday, Saturday, Sunday }
+public enum Days {None, Moday, Tuesday, Wednessday, Thursday, Friday, Saturday, Sunday }
+
 public class Class1
 {
+    public static string DaysOfTheWeek (int dayOne, int dayTwo, int dayThree)
+    {
+
+
+
+
+
+
+        return DaysOfTheWeek ((Days)dayTwo);
+    }
+
+	public static string DaysOfTheWeek (int day)
+    {
+
+
+
+
+
+
+    }
+
+
 	
+}
+public class Program
+{
+    static void Main (string[] args )
+    {
+
+    }
 }
